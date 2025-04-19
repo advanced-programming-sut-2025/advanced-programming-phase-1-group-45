b7ad4adf70a7182ac7e5a4c8c5a2f986a8a69502
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-04-19 07:46:51 +0330

    Update UserManager.java

--- Managers/UserManager.java
+++ Managers/UserManager.java
@@ -8,5 +8,11 @@ public class UserManager {
     //getUserinfo
     //updateProfile
     //load
-    //save same as game
+    //save same as gameManager
+    private void save(){
+        try(Writer w = Files.newBufferedWriter(storage)){
+            gson.toJson(gameSessions, w);
+        } catch (IOException ignored) {
+        }
+    }
 }
