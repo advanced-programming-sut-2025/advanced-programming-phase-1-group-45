a38890f45bfd6d13573828d405422dd9a7a30057
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-04-26 00:26:02 +0330

    Update LoginMenu.java

--- views/LoginMenu.java
+++ views/LoginMenu.java
@@ -7,17 +7,35 @@ public class LoginMenu implements Menu{
     @Override
     public void handleCommand(String command, MenuController controller){
         if(command.startsWith("login")){
-            User u = controller.getUserManager().login(command));
+            User u = controller.getUserManager().login(command);
             if(u != null){
                 controller.setCurrentUser(u);
                 System.out.println("Login Successful, moving to main menu");
                 controller.setCurrentMenu(new MainMenu());
             }
         } else if(command.startsWith("forget password")){
-            controller.getUserManager().startPasswordRecovery(command);
+            String um = controller.getUserManager().startPasswordRecovery(command);
+            controller.setPendingRecoveryUsername(um);
         } else if(command.startsWith("answer")){
             controller.getUserManager().completePasswordRecovery(command);
-        } else if(command.equals("show current menu"){
+        } else if(command.equals("reset random")){
+            String user = controller.getPendingRecoveryUsername();
+            if(user != null){
+                String newPassword = controller.getUserManager().resetPasswordRandom(user);
+                System.out.println("your new password is: " + newPassword);
+                controller.setPendingRecoveryUsername(null);
+            }
+        } else if(command.startsWith("reset set -p ")){
+            String user = controller.getPendingRecoveryUsername();
+            if(user != null){
+                String newPassword = command.substring("reset set -p ".length());
+                if(controller.getUserManager().resetPasswordManual(user, newPassword)){
+                    System.out.println("Password reset successful");
+                    controller.setPendingRecoveryUsername(null);
+                }
+            }
+        }
+        else if(command.equals("show current menu")){
             System.out.println("you are now in Login Menu");
         } else if(command.equals("menu exit")){
             System.out.println("Goodbye");
