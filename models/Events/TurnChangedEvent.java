9d3ee6d987b695727433bf411cf9ad4386c6268b
ASUS <behzadi20001@gmail.com>
2025-05-04 22:50:35 +0330

    update files

--- models/Events/TurnChangedEvent.java
+++ models/Events/TurnChangedEvent.java
@@ -3,4 +3,22 @@ package models.Events;
 import models.Enums.Season;
 
 public class TurnAdvancedEvent(int hour, int day, Season season) {
+    private int hour;
+    private int day;
+    private Season season;
+
+    TurnAdvancedEvent(int hour, int day, Season season) {
+        this.hour = hour;
+        this.day = day;
+        this.season = season;
+    }
+    public int getHour() {
+        return hour;
+    }
+    public int getDay() {
+        return day;
+    }
+    public Season getSeason() {
+        return season;
+    }
 }
--- models/Events/WeatherChangedEvent.java
+++ models/Events/WeatherChangedEvent.java
@@ -3,6 +3,26 @@ package models.Events;
 import models.Enums.Season;
 import models.Enums.Weather;
 
-public record WeatherChangedEvent(Weather newWeather, Weather previousWeather,
-                                  Season season) {
+public record WeatherChangedEvent {
+    private Weather newWeather;
+    private Weather oldWeather;
+    private Season season;
+
+    WeatherChangedEvent(Weather newWeather, Weather oldWeather, Season season) {
+        this.newWeather = newWeather;
+        this.oldWeather = oldWeather;
+        this.season = season;
+    }
+
+    public Weather getNewWeather() {
+        return newWeather;
+    }
+
+    public Weather getOldWeather() {
+        return oldWeather;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
 }
