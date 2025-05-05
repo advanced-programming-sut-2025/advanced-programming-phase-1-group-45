cbe4dfb1ff2c0749891e3986601fb2d0f336dc9e
ASUS <behzadi20001@gmail.com>
2025-05-05 12:37:15 +0330

    add events

---
+++ src/main/java/models/crops/Crop.java
@@ -0,0 +1,67 @@
+package models.crops;
+
+import models.Enums.Season;
+
+public class Crop {
+    private final String name;
+    private final String source;
+    private final int[] stages;
+    private final int totalHarvestTime;
+    private final boolean isOneTime;
+    private final int regrowTime;
+    private final long baseSellPrice;
+    private final boolean isEdible;
+    private final int energy;
+    private final Season[] season;
+    private final boolean canBecomeGiant;
+
+    Crop(String name, String source, int[] stages,
+         int totalHarvestTime, boolean isOneTime, int regrowTime,
+         long baseSellPrice, boolean isEdible, int energy,
+         Season[] season, boolean canBecomeGiant) {
+        this.name = name;
+        this.source = source;
+        this.stages = stages;
+        this.totalHarvestTime = totalHarvestTime;
+        this.isOneTime = isOneTime;
+        this.regrowTime = regrowTime;
+        this.baseSellPrice = baseSellPrice;
+        this.isEdible = isEdible;
+        this.energy = energy;
+        this.season = season;
+        this.canBecomeGiant = canBecomeGiant;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSource() {
+        return source;
+    }
+    public int[] getStages() {
+        return stages;
+    }
+    public int getTotalHarvestTime() {
+        return totalHarvestTime;
+    }
+    public boolean isOneTime() {
+        return isOneTime;
+    }
+    public int getRegrowTime() {
+        return regrowTime;
+    }
+    public long getBaseSellPrice() {
+        return baseSellPrice;
+    }
+    public boolean isEdible() {
+        return isEdible;
+    }
+    public int getEnergy() {
+        return energy;
+    }
+    public Season[] getSeason() {
+        return season;
+    }
+    public boolean isCanBecomeGiant() {
+        return canBecomeGiant;
+    }
+}
