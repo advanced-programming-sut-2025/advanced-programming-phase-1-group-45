5347882ca02a46d66c3cc34162a5a87a1d58b71f
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-05-09 17:55:02 +0330

    Update MenuController.java

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import managers.GameManager;
 import managers.ShopManager;
 import managers.UserManager;
@@ -7,37 +8,28 @@ import models.GameSession;
 import views.Menu;
 import views.StartMenu;
 
-import java.util.*;
-
 public class MenuController {
     private Menu currentMenu = new StartMenu();
-    private Scanner scanner = new Scanner(System.in);
     private GameManager gameManager = new GameManager();
     private UserManager userManager = new UserManager();
     private ShopManager shopManager = new ShopManager(userManager);
     private User currentUser;
     private GameSession currentSession;
-    private String pendingRecoveryUsername = null;
+    private String pendingRecoveryUsername;
 
-    public ShopManager getShopManager() {
-        return shopManager;
+    public void processCommand(String command) {
+        currentMenu.handleCommand(command, this);
     }
 
-    public UserManager getUserManager() {return userManager;}
-    public GameManager getGameManager() {return gameManager;}
-    public User getCurrentUser() {return currentUser;}
-    public GameSession getCurrentSession() {return currentSession;};
-    public String getPendingRecoveryUsername() {return pendingRecoveryUsername;}
+    public ShopManager getShopManager() { return shopManager; }
+    public UserManager getUserManager() { return userManager; }
+    public GameManager getGameManager() { return gameManager; }
+    public User getCurrentUser() { return currentUser; }
+    public GameSession getCurrentSession() { return currentSession; }
+    public String getPendingRecoveryUsername() { return pendingRecoveryUsername; }
 
-    public void setCurrentUser(User u) {this.currentUser = u;}
-    public void setCurrentMenu(Menu menu) {this.currentMenu = menu;}
-    public void setCurrentSession(GameSession s) {this.currentSession = s;}//save user, map
-    public void setPendingRecoveryUsername(String s) {this.pendingRecoveryUsername = s;}
-    public void run(){
-        while(true){
-            System.out.println("> ");
-            String line = scanner.nextLine();
-            currentMenu.handleCommand(line, this);
-        }
-    }
+    public void setCurrentUser(User u) { this.currentUser = u; }
+    public void setCurrentMenu(Menu menu) { this.currentMenu = menu; }
+    public void setCurrentSession(GameSession s) { this.currentSession = s; }
+    public void setPendingRecoveryUsername(String s) { this.pendingRecoveryUsername = s; }
 }
