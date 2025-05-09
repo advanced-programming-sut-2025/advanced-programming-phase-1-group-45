edde1baa46c8d8fdd8256a0401806a67e181393f
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-05-10 01:08:59 +0330

    Update MenuController.java

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -3,10 +3,15 @@ package controllers;
 import managers.GameManager;
 import managers.ShopManager;
 import managers.UserManager;
+import models.GiftLogEntry;
 import models.User;
 import models.GameSession;
 import views.Menu;
 import views.StartMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class MenuController {
     private Menu currentMenu = new StartMenu();
@@ -16,6 +21,17 @@ public class MenuController {
     private User currentUser;
     private GameSession currentSession;
     private String pendingRecoveryUsername;
+    private List<GiftLogEntry> giftLogs = new ArrayList<>();
+    private int giftIdCounter = 1;
+    public Optional<GiftLogEntry> getGiftById(int id, String viewer){
+        return giftLogs.stream().filter(g -> g.getId() == id && (g.sender.equals(viewer) || g.receiver.equals(viewer))).findFirst();
+    }
+    public void recordGift(String sender, String receiver, String item, int amount){
+        giftLogs.add(new GiftLogEntry(giftIdCounter++, sender, receiver, item, amount));
+    }
+    public List<GiftLogEntry> getGiftLogs(){
+        return giftLogs;
+    }
 
     public void processCommand(String command) {
         currentMenu.handleCommand(command, this);
