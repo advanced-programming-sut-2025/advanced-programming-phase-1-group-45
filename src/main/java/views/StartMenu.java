01e199d65bf7e654227a46435f25d5b7d34b10b9
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-05-13 02:39:40 +0330

    Update StartMenu.java

--- src/main/java/views/StartMenu.java
+++ src/main/java/views/StartMenu.java
@@ -5,22 +5,39 @@ import controllers.MenuController;
 public class StartMenu implements Menu{
     @Override
     public void handleCommand(String command, MenuController controller) {
-        switch (command) {
-            case "menu enter register":
-                controller.setCurrentMenu(new SignUpMenu());
-                break;
-            case "menu enter login":
-                controller.setCurrentMenu(new LoginMenu());
-                break;
-            case "show current menu":
-                System.out.println("welcome");
-                break;
-            case "menu exit":
-                System.out.println("Bye");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid command");
+        if (command.equals("menu enter register")){
+            System.out.println("Register Menu");
+            controller.setCurrentMenu(new SignUpMenu());
+        }else if(command.equals("menu enter login")){
+            System.out.println("Login Menu");
+            controller.setCurrentMenu(new LoginMenu());
+        }else if(command.equals("show current menu")){
+            System.out.println("welcome");
+        }else if(command.equals("menu exit")){
+            System.out.println("Bye");
+            System.exit(0);
+        }else if(command.startsWith("forget password")){
+            String um = controller.getUserManager().startPasswordRecovery(command);
+            controller.setPendingRecoveryUsername(um);
+        } else if(command.startsWith("answer")){
+            controller.getUserManager().completePasswordRecovery(command);
+        } else if(command.equals("reset random")){
+            String user = controller.getPendingRecoveryUsername();
+            if(user != null){
+                String newPassword = controller.getUserManager().resetPasswordRandom(user);
+                System.out.println("your new password is: " + newPassword);
+                controller.setPendingRecoveryUsername(null);
+            }
+        } else if(command.startsWith("reset set -p ")){
+            String user = controller.getPendingRecoveryUsername();
+            if(user != null){
+                String newPassword = command.substring("reset set -p ".length());
+                if(controller.getUserManager().resetPasswordManual(user, newPassword)){
+                    System.out.println("Password reset successful");
+                    controller.setPendingRecoveryUsername(null);
+                }
+            }
         }
+        else System.out.println("Invalid command");
     }
 }
