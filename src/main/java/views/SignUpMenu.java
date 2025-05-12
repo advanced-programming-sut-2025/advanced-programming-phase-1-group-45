e69108e7101905d014f0e044868a7ed918d4700a
HannanehSheykhi <hananeh.sheykhi1385@gmail.com>
2025-05-13 02:40:04 +0330

    Update SignUpMenu.java

--- src/main/java/views/SignUpMenu.java
+++ src/main/java/views/SignUpMenu.java
@@ -6,10 +6,12 @@ public class SignUpMenu implements Menu{
     @Override
     public void handleCommand(String command, MenuController controller) {
         if(command.startsWith("register ")) {
-            controller.getUserManager().register(command);
+            controller.getUserManager().register(command, controller);
         } else if (command.startsWith("-a")){
-            controller.getUserManager().setAnswer(command);
+            String answer = command.split("\\s+")[1];
+            controller.getCurrentUser().setSecurityAnswer(answer);
             System.out.println("User registered successfully");
+            System.out.println("you are now in main menu");
             controller.setCurrentMenu(new MainMenu());
         } else if(command.equals("show current menu")){
             System.out.println("You are now in sign up menu");
