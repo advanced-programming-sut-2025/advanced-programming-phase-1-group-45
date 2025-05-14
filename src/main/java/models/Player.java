784f643dc528b0284387ba4fe873d00abaa17f57
ASUS <behzadi20001@gmail.com>
2025-05-14 22:49:50 +0330

    last change

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,37 +1,109 @@
 package models;
 
-import models.MapElements.Tile.Tile;
-import models.Tools.Backpack.Backpack;
-
 public class Player {
-    private UserInfo UserInfo;
-    private Backpack backpack = new Backpack();
-    private int PlayerX;
-    private int PlayerY;
-    private Tile currentTile;
     private Energy energy;
+    private boolean isCollapsed;
+    private String equippedTool;
+    private String backpackType;
+    private int backpackCapacity;
+    private String trashCanType;
 
-    public UserInfo getUserInfo() {
-        return UserInfo;
+    public Player(int initialEnergy) {
+        this.energy = new Energy(initialEnergy);
+        this.isCollapsed = false;
+        this.equippedTool = null;
+        this.backpackType = "Basic Backpack";
+        this.backpackCapacity = 12;
+        this.trashCanType = "Basic Trash Can";
     }
 
-    public Backpack getInventory() {
-        return backpack;
+    public String showEnergy() {
+        return energy.showEnergy();
     }
 
-    public int getPlayerX() {
-        return PlayerX;
+    public boolean consumeEnergy(int amount) {
+        boolean success = energy.consumeEnergy(amount);
+        if (!success && amount > 0) {
+            collapse();
+        }
+        return success;
     }
 
-    public int getPlayerY() {
-        return PlayerY;
+    public void setEnergy(int value) {
+        energy.setEnergy(value);
     }
 
-    public Tile getCurrentTile() {
-        return currentTile;
+    public void setUnlimitedEnergy() {
+        energy.setUnlimitedEnergy();
+    }
+
+    public void collapse() {
+        isCollapsed = true;
+        energy.collapse();
+        System.out.println("You've collapsed due to exhaustion!");
+    }
+
+    public void restoreEnergyForNewDay() {
+        energy.restoreEnergyForNewDay(isCollapsed);
+        isCollapsed = false;
+    }
+
+    public void equipTool(String toolName) {
+        this.equippedTool = toolName;
+    }
+
+    public String getCurrentTool() {
+        return equippedTool;
+    }
+
+    public void upgradeBackpack(String newType) {
+        this.backpackType = newType;
+        switch (newType) {
+            case "Large Backpack":
+                this.backpackCapacity = 24;
+                break;
+            case "Deluxe Backpack":
+                this.backpackCapacity = Integer.MAX_VALUE;
+                break;
+        }
+    }
+
+    public void upgradeTrashCan(String newType) {
+        this.trashCanType = newType;
+    }
+
+    public double getTrashCanReturnRate() {
+        switch (trashCanType) {
+            case "Copper Trash Can":
+                return 0.15;
+            case "Iron Trash Can":
+                return 0.30;
+            case "Gold Trash Can":
+                return 0.45;
+            case "Iridium Trash Can":
+                return 0.60;
+            default:
+                return 0.0;
+        }
+    }
+
+    public int getBackpackCapacity() {
+        return backpackCapacity;
+    }
+
+    public boolean isCollapsed() {
+        return isCollapsed;
     }
 
     public Energy getEnergy() {
         return energy;
+    }
+
+    public String getBackpackType() {
+        return backpackType;
+    }
+
+    public String getTrashCanType() {
+        return trashCanType;
     }
 }
