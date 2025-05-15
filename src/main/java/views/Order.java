0137ba968b8179cc62c25f2225124bb9e32eaeb6
mahshid <MAHSHIDKARIMI3590@GMAIL.COM>
2025-05-15 13:41:31 +0330

    Update Order.java

--- src/main/java/views/Order.java
+++ src/main/java/views/Order.java
@@ -1,5 +1,6 @@
 package views;
 
+import models.Player;
 import models.User;
 
 import java.util.List;
@@ -29,6 +30,69 @@ public class Order {
             System.out.println("Successfully crafted " + recipeName);
         } else {
             System.out.println("Failed to craft " + recipeName);
+        }
+    }
+
+
+    public void handleArtisanUse(String[] args) {
+        
+        if (args.length < 3) {
+            System.out.println("Usage: artisan use <machine_name> <item_name>");
+            return;
+        }
+
+        String machineName = args[1];
+        String itemName = args[2];
+
+        if (Player.useArtisanMachine(machineName, itemName)) {
+            System.out.println("Started processing " + itemName + " in " + machineName);
+        } else {
+            System.out.println("Failed to use " + machineName);
+        }
+    }
+
+    
+    public void handleArtisanGet(String[] args) {
+      
+        if (args.length < 2) {
+            System.out.println("Usage: artisan get <machine_name>");
+            return;
+        }
+
+        String machineName = args[1];
+        String product = Player.getArtisanProduct(machineName);
+
+        if (product != null) {
+            System.out.println("Harvested " + product + " from " + machineName);
+        } else {
+            System.out.println("Nothing to harvest from " + machineName);
+        }
+    }
+
+   
+    public void handleArtisanStatus(String[] args) {
+        // artisan status <machine_name>
+        if (args.length < 2) {
+            System.out.println("Usage: artisan status <machine_name>");
+            return;
+        }
+
+        String machineName = args[1];
+        System.out.println(Player.getArtisanMachineStatus(machineName));
+    }
+
+    
+    public void handleArtisanList(String[] args) {
+        
+        List<String> machines =Player.listArtisanMachines();
+
+        if (machines.isEmpty()) {
+            System.out.println("You don't have any artisan machines.");
+        } else {
+            System.out.println("=== Your Artisan Machines ===");
+            for (String machine : machines) {
+                System.out.println(machine);
+            }
         }
     }
 }
