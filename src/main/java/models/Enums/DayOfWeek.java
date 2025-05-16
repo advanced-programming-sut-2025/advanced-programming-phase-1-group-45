301c23bed1727284a6be462b39a9c2bb4ce996f6
FatemehBehzadi <behzadi20001@gmail.com>
2025-05-16 12:00:36 +0330

    Update DayOfWeek.java

--- src/main/java/models/Enums/DayOfWeek.java
+++ src/main/java/models/Enums/DayOfWeek.java
@@ -2,4 +2,16 @@ package models.Enums;
 
 public enum DayOfWeek {
     Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday;
+    public DayOfWeek next() {
+        return switch (this) {
+            case Saturday -> Sunday;
+            case Sunday -> Sunday;
+            case Monday -> Monday;
+            case Tuesday -> Tuesday;
+            case Wednesday -> Wednesday;
+            case Thursday -> Thursday;
+            case Friday -> Friday;
+            default -> null;
+        };
+    }
 }
