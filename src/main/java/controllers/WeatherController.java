f95d68311b2669f984e65f328a1ae82d7abde0fe
FatemehBehzadi <behzadi20001@gmail.com>
2025-05-16 12:01:43 +0330

    Update WeatherController.java

--- src/main/java/controllers/WeatherController.java
+++ src/main/java/controllers/WeatherController.java
@@ -8,12 +8,15 @@ import models.Events.GameEventBus;
 import models.Events.SeasonChangedEvent;
 import models.Events.WeatherChangedEvent;
 import managers.TimeManager;
+import models.GameMap;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class WeatherController {
     private static final WeatherController instance = new WeatherController();
-    private Weather currentWeather;
+    private Weather currentWeather = Weather.SUNNY;
+    private Weather forecastWeather;
 
     public static WeatherController getInstance() {
         return instance;
@@ -23,32 +26,58 @@ public class WeatherController {
         return currentWeather;
     }
 
+    public void setCurrentWeather(String weather) {
+        if (weather.equalsIgnoreCase("SNOWY")) {
+            currentWeather = Weather.SNOWY;
+        } else if (weather.equalsIgnoreCase("Rainy")) {
+            currentWeather = Weather.RAINY;
+        } else if(weather.equalsIgnoreCase("Stormy")) {
+            currentWeather = Weather.STORMY;
+        } else if (weather.equalsIgnoreCase("Sunny")) {
+            currentWeather = Weather.SUNNY;
+        } else {
+            System.out.println("invalid weather");
+        }
+    }
+
     public WeatherController() {
         GameEventBus.INSTANCE.register(this);
         // Initialize weather
         Season firstSeason = TimeManager.getInstance().getSeason();
-        randomlyChangeWeather(firstSeason);
-        //    System.out.println(currentWeather.toString());
+        randomlyChangeWeatherForTomorrow(firstSeason);
     }
-
-    @Subscribe
-    public void newWeatherForNewSeason(SeasonChangedEvent event) {
-        randomlyChangeWeather(event.newSeason());
-        System.out.println(currentWeather.toString());
-    }
+//
+//    @Subscribe
+//    public void newWeatherForNewSeason(SeasonChangedEvent event) {
+//        randomlyChangeWeatherForTomorrow(event.newSeason());
+//        System.out.println(currentWeather.toString());
+//    }
 
     @Subscribe
     public void onDayChanged(DayChangedEvent event) {
         Weather previousWeather = currentWeather;
-        randomlyChangeWeather(event.season());
+        currentWeather = forecastWeather;
+        if (TimeManager.getInstance().getDay() % 27 == 0) {
+            randomlyChangeWeatherForTomorrow(event.season().next());
+        } else {
+            randomlyChangeWeatherForTomorrow(event.season());
+        }
         GameEventBus.INSTANCE.post(new WeatherChangedEvent(currentWeather, previousWeather,
                 event.season()));
         //  System.out.println(currentWeather.toString() + " " + event.season().toString());
-
     }
 
-    private void randomlyChangeWeather(Season season) {
+    private void randomlyChangeWeatherForTomorrow(Season season) {
         Weather[] weathers = season.getWeathers();
-        currentWeather = weathers[new Random().nextInt(weathers.length)];
+        forecastWeather = weathers[new Random().nextInt(weathers.length)];
     }
+
+    public Weather getForecastWeather() {
+        return forecastWeather;
+    }
+
+//    public void thor(WeatherChangedEvent event) {
+//        if (event.newWeather().equals(Weather.STORMY))
+//        }
+//    }
 }
