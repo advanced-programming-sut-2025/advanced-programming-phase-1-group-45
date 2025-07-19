2a76404f82c981d8c8029a968c7003d61e8bc88e
Hana <hananeh.sheykhi1385@gmail.com>
2025-07-19 17:40:45 +0330

    Update PasswordGenerator.java
    change random password logic

--- core/src/main/java/com/proj/Model/PasswordGenerator.java
+++ core/src/main/java/com/proj/Model/PasswordGenerator.java
@@ -1,14 +1,39 @@
-// PasswordGenerator.java
 package com.proj.Model;
 
 public class PasswordGenerator {
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()_+";
+
     public static String generateRandomPassword() {
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
         StringBuilder password = new StringBuilder();
-        for (int i = 0; i < 12; i++) {
-            int index = (int) (Math.random() * chars.length());
-            password.append(chars.charAt(index));
+        String allChars = UPPER + LOWER + DIGITS + SPECIAL;
+
+        password.append(randomChar(UPPER));
+        password.append(randomChar(LOWER));
+        password.append(randomChar(DIGITS));
+        password.append(randomChar(SPECIAL));
+
+        for (int i = 0; i < 8; i++) { 
+            password.append(randomChar(allChars));
         }
-        return password.toString();
+
+        return shuffleString(password.toString());
+    }
+
+    private static char randomChar(String characterSet) {
+        return characterSet.charAt((int) (Math.random() * characterSet.length()));
+    }
+
+    private static String shuffleString(String input) {
+        char[] characters = input.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            int randomIndex = (int) (Math.random() * characters.length);
+            char temp = characters[i];
+            characters[i] = characters[randomIndex];
+            characters[randomIndex] = temp;
+        }
+        return new String(characters);
     }
 }
