ca3bcee2ab4b88d0afb80ca9f2a1e40ed8feda0a
ASUS <behzadi20001@gmail.com>
2025-07-20 16:38:51 +0330

    update farmName.java

--- core/src/main/java/com/proj/map/farmName.java
+++ core/src/main/java/com/proj/map/farmName.java
@@ -1,4 +1,30 @@
 package com.proj.map;
 
 public enum farmName {
+    STANDARD("Standard", "A simple plot of land, with a large amount of open space to design your farm."),
+    RIVERLAND("Riverland", "Your farm is spread across several islands and scenic riverbanks." +
+            " Fish are more common here than usual. You start with a fish smoker."),
+    FOREST("Forest", "The woods limit your farming space." +
+            " However, the bounty of the forest is nearly at your doorstep."),
+    HILL_TOP("Hill_top", "Rocky terrain and a winding river make it difficult to design your farm." +
+            "However, a mineral deposit provides mining opportunities."),
+    WILDERNESS("Wilderness", "There's plenty of good land here, but beware... at night the monsters come out."),
+    FOUR_CORNERS("FourCorners", "The land is divide into four parcels, each with its own perk." +
+            "Perfect for a group!");
+
+    private String farmName;
+    private String description;
+
+    farmName(String farmName, String description) {
+        this.farmName = farmName;
+        this.description = description;
+    }
+
+    public String getFarmName() {
+        return farmName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
