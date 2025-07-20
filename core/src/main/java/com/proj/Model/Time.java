a6c78be0ac02009ab42500a15994beee68327a8c
ASUS <behzadi20001@gmail.com>
2025-07-21 00:02:37 +0330

    add time.java, Weather.java

--- core/src/main/java/com/proj/Model/Time.java
+++ core/src/main/java/com/proj/Model/Time.java
@@ -1,4 +1,19 @@
 package com.proj.Model;
 
+import com.proj.map.Season;
+
 public class Time {
+    private int day;
+    private int hour;
+    private Season season;
+    private Weather weather;
+
+    //initialize
+    Time() {
+        day = 1;
+        hour = 9;
+        season = Season.SPRING;
+        weather = Weather.SUNNY;
+    }
+
 }
--- core/src/main/java/com/proj/Model/Weather.java
+++ core/src/main/java/com/proj/Model/Weather.java
@@ -1,4 +1,15 @@
 package com.proj.Model;
 
 public enum Weather {
+    SUNNY("Sunny"),
+    SNOWY("Snowy"),
+    RAINY("Rainy"),
+    WINDY("Windy");
+    private String weather;
+    Weather(String weather) {
+        this.weather = weather;
+    }
+    public String getWeather() {
+        return weather;
+    }
 }
