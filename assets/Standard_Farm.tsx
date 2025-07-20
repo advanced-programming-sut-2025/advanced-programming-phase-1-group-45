c95c8c6a9bca8546e6053befc267f9322b27ed22
ASUS <behzadi20001@gmail.com>
2025-07-20 13:21:08 +0330

    assets update

--- assets/Standard_Farm.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Standard_Farm" tilewidth="16" tileheight="16" tilecount="5200" columns="80">
- <image source="Standard_Farm.png" width="1280" height="1040"/>
-</tileset>
