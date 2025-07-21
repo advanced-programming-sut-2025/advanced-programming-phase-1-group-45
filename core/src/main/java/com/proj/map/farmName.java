bb87bf795d40f470ac8e76414554cb8925c5da66
FatemehBehzadi <behzadi20001@gmail.com>
2025-07-21 17:09:40 +0330

    Update farmName.java

--- core/src/main/java/com/proj/map/farmName.java
+++ core/src/main/java/com/proj/map/farmName.java
@@ -4,13 +4,17 @@ public enum farmName {
     STANDARD("Standard", "A simple plot of land, with a large amount of open space to design your farm."),
     RIVERLAND("Riverland", "Your farm is spread across several islands and scenic riverbanks." +
             " Fish are more common here than usual. You start with a fish smoker."),
-    FOREST("Forest", "The woods limit your farming space." +
+    FOREST("ForestLand", "The woods limit your farming space." +
             " However, the bounty of the forest is nearly at your doorstep."),
     HILL_TOP("Hill_top", "Rocky terrain and a winding river make it difficult to design your farm." +
             "However, a mineral deposit provides mining opportunities."),
     WILDERNESS("Wilderness", "There's plenty of good land here, but beware... at night the monsters come out."),
     FOUR_CORNERS("FourCorners", "The land is divide into four parcels, each with its own perk." +
-            "Perfect for a group!");
+            "Perfect for a group!"),
+    MEADOWLANDS("Meadowlands", "It's not the best for growing crops, but there's a chewy blue grass" +
+        "that animals love. You start with two chickens."),
+    BEACH("Island", "Good foraging and fishing, and tons of open space. Sometimes, supply" +
+        "crates wash up on share. However, sprinklers don't work in the sandy soil.");
 
     private String farmName;
     private String description;
