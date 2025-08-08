9ea987ee9eb07060603a6ed91a0974e58db2ae0a
Hana <hananeh.sheykhi1385@gmail.com>
2025-08-08 22:33:57 +0330

    Update LoginMenuController.java

--- core/src/main/java/com/proj/Control/LoginMenuController.java
+++ core/src/main/java/com/proj/Control/LoginMenuController.java
@@ -7,89 +7,55 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.proj.Main;
 import com.proj.View.*;
 import com.proj.Model.*;
+import com.proj.network.lobby.LobbyScreen;
+import com.proj.Database.DatabaseHelper;
 import com.proj.network.client.NetworkEventListener;
 import com.proj.network.event.NetworkEvent;
-import com.proj.network.lobby.LobbyScreen;
 
 public class LoginMenuController {
     private LoginMenuView view;
     private Skin skin;
+    private final DatabaseHelper dbHelper = new DatabaseHelper();
+
 
     public void setView(LoginMenuView view) {
         this.view = view;
         this.skin = GameAssetManager.getGameAssetManager().getSkin();
+        dbHelper.connect();
     }
 
     public void handleForgetPasswordButton() {
-        if (view.getForgotPasswordButton().isChecked()) {
-            Main.getMain().getScreen().dispose();
-            Main.getMain().setScreen(new ForgetPasswordView(new ForgetPasswordController(), skin));
-        }
+        // Fixed: Use proper click listener instead of isChecked()
+        view.getForgotPasswordButton().addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Main.getMain().getScreen().dispose();
+                Main.getMain().setScreen(new ForgetPasswordView(new ForgetPasswordController(), skin));
+            }
+        });
     }
 
-    /*public void handleLoginButton() {
+    // In LoginMenuController
+    public void handleLoginButton() {
         view.getLoginButton().addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                String username = view.getUsername().getText();
+                String identifier = view.getUsername().getText();
                 String password = view.getPassword().getText();
 
-                if (login(username, password)) {
-                    Main.getMain().switchToGameScreen();
+                User authenticatedUser = dbHelper.authenticateUser(identifier, password);
+                if (authenticatedUser != null) {
+                    // Proceed to game screen with authenticated user
+                    Main.getMain().setScreen(new LobbyScreen(Main.getMain()));
+                    //Main.getMain().setScreen(new MainMenuView(new MainMenuController(), skin));
                 } else {
-                    view.getErrorMessage().setText("Invalid credentials!");
+                    view.getErrorMessage().setText("Invalid credentials");
                 }
             }
         });
-    }*/
-    public void handleLoginButton() {
-        if (view != null) {
-            view.getLoginButton().addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-                    String username = view.getUsername().getText();
-                    String password = view.getPassword().getText();
-
-                    if (login(username, password)) {
-//                        // After successful login, go to lobby screen
-//                        // After successful login
-                        Main.getMain().setScreen(new LobbyScreen(Main.getMain()));
-//                       // Main.getMain().switchToGameScreen();// on github, not a comment
-                    }
-                }
-            });
-        }
     }
-
-
-    /*public void handleLoginButton() {
-        if (view.getLoginButton().isChecked()) {
-            String username = view.getUsername().getText();
-            String password = view.getPassword().getText();
-
-            if (login(username, password)) {
-                Main.getMain().getScreen().dispose();
-                Main.getMain().setScreen(new PreGameMenuView(new PreGameMenuController(), skin));
-            }
-        }
-    }*/
-
-    private boolean login(String username, String password) {
-        if (!Authenticator.existsUsername(username)) {
-            view.getErrorMessage().setText("Username not found.");
-            return false;
-        }
-        else {
-            User user = Authenticator.authenticate(username, password);
-
-            if (user == null) {
-                view.getErrorMessage().setText("Wrong password.");
-                return false;
-            }
-        }
-
-        return true;
-    }
-
 }
+//Main.getMain().setScreen(new LobbyScreen(Main.getMain()));
+
+
 
