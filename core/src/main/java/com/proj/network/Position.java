289cca82ed10c1d0e8cfe49942898c843839518f
FatemehBehzadi <behzadi20001@gmail.com>
2025-08-14 18:11:57 +0330

    Update Position.java

--- core/src/main/java/com/proj/network/Position.java
+++ core/src/main/java/com/proj/network/Position.java
@@ -3,21 +3,12 @@ package com.proj.network;
 public class Position {
     private float x;
     private float y;
-
     public Position(float x, float y) {
         this.x = x;
         this.y = y;
     }
-
     public float getX() { return x; }
     public void setX(float x) { this.x = x; }
-
     public float getY() { return y; }
     public void setY(float y) { this.y = y; }
-
-    public float distanceTo(Position other) {
-        float dx = this.x - other.x;
-        float dy = this.y - other.y;
-        return (float) Math.sqrt(dx * dx + dy * dy);
-    }
 }
