df0cfd04258d5c1db9f8132f1d41282ed56dff4f
Hana <hananeh.sheykhi1385@gmail.com>
2025-08-14 04:22:59 +0330

    Update GameScreen.java

--- core/src/main/java/com/proj/GameScreen.java
+++ core/src/main/java/com/proj/GameScreen.java
@@ -50,6 +50,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import com.proj.Radio.RadioMenuScreen;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.proj.View.ScoreboardView;
 
 import java.awt.*;
 
@@ -883,6 +884,23 @@ public class GameScreen implements Screen, ChatListener {
             if (Gdx.input.isKeyJustPressed(Input.Keys.U)) {
                 showButtonImage = !showButtonImage;
                 Gdx.app.log("GameScreen", "U key pressed. Show image: " + showButtonImage);
+            }
+            if (Gdx.input.isKeyJustPressed(Input.Keys.G)) {
+                Gdx.app.postRunnable(() -> {
+                    try {
+                        ScoreboardController controller = new ScoreboardController(main.getDatabaseHelper());
+                        ScoreboardView scoreboardScreen = new ScoreboardView(controller);
+
+                        scoreboardScreen.setBackCallback(() -> {
+                            main.setScreen(this);
+                            Gdx.input.setInputProcessor(stage);
+                        });
+
+                        main.setScreen(scoreboardScreen);
+                    } catch (Exception e) {
+                        Gdx.app.error("GameScreen", "Error opening scoreboard", e);
+                    }
+                });
             }
 
             if (Gdx.input.isKeyJustPressed(Input.Keys.E)) {
